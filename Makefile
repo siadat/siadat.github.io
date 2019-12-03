d3924e8484d4a323f4a7e4b2a6e5b92dda5d30e8
Sina Siadat <siadat@gmail.com>
2019-12-03 22:38:33 +0330

    rename books.html to reading.html

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 all: blog homepage
 
-homepage: projects.yaml books.yaml
+homepage: projects.yaml readings.yaml
 	poetry run python generate.py
 
 blog: dependencies
@@ -14,23 +14,9 @@ clean:
 	rm -rf blog/index.html
 	rm -rf blog/index.xml
 	rm -f index.html
-	rm -f books.json
-	rm -f projects.json
 
 dependencies:
 	# go get github.com/siadat/blgo
-
-projects.json:
-	node fetch.js projects | jq . > projects.json
-
-projects.yaml:
-	# cat projects.json | poetry run python json-to-yaml.py > projects.yaml 
-
-books.json:
-	node fetch.js books | jq . > books.json
-
-books.yaml:
-	# cat books.json | poetry run python json-to-yaml.py > books.yaml 
 
 header-open:
 	vimdiff +/header +':windo normal ggnzt\<cr>' header.html blog/templates/index.tmpl.html blog/templates/post.tmpl.html
--- generate.py
+++ generate.py
@@ -24,19 +24,16 @@ for templateFilename in glob.glob("*.template.html"):
     template = env.get_template(templateFilename)
 
     projectsFile = open("projects.yaml")
-    booksFile = open("books.yaml")
-
-    # projects = json.load(projectsFile)
-    # books = json.load(booksFile)
+    readingsFile = open("readings.yaml")
 
     projects = yaml.load(projectsFile)
-    books = yaml.load(booksFile)
+    readings = yaml.load(readingsFile)
 
     projects = filter(lambda b: "order" in b and b["order"] != "", projects)
     projects = sorted(projects, key = (lambda b: int(b["order"])), reverse = False)
 
-    books = filter(lambda b: "weight" in b and int(b["weight"]) >= 3, books)
-    books = sorted(books, key = (lambda b: int(b["weight"])), reverse = True)
+    readings = filter(lambda b: "weight" in b and int(b["weight"]) >= 3, readings)
+    readings = sorted(readings, key = (lambda b: int(b["weight"])), reverse = True)
 
     outputFile = open(outputFilename, "w")
-    outputFile.write(template.render(projects=projects, books=books, header=headerContent, aboutme=aboutmeContent).encode('utf-8'))
+    outputFile.write(template.render(projects=projects, readings=readings, header=headerContent, aboutme=aboutmeContent).encode('utf-8'))
